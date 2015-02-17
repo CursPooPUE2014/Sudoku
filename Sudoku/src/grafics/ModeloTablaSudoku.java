79890b3b4732f3991a23038697552024012dca65
quermd <quermd@gmail.com>
2015-02-17 11:46:05 +0100

    Projecte Inicial

---
+++ Sudoku/src/grafics/ModeloTablaSudoku.java
@@ -0,0 +1,45 @@
+package grafics;
+
+
+import java.util.Arrays;
+import java.util.Random;
+
+import javax.swing.table.AbstractTableModel;
+
+import sun.rmi.runtime.Log;
+
+import java.util.logging.Logger;
+
+
+public class ModeloTablaSudoku extends AbstractTableModel{
+
+	private static final boolean DEBUG = false;
+	
+	private int[][] cuadricula;
+		
+	
+	public ModeloTablaSudoku(){
+		
+		GenerarSudoku sudokuInicial = new GenerarSudoku();
+		this.cuadricula= sudokuInicial.getCuadricula();
+	}
+
+	@Override
+	public int getRowCount() {
+		
+		return cuadricula.length;
+	}
+
+	@Override
+	public int getColumnCount() {
+		
+		return cuadricula[0].length;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		
+		return cuadricula[rowIndex][columnIndex];
+	}
+	
+}
